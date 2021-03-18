d260c8cb51c1d62d1216077c03f5dfa391f49961
unknown <csm1165@o365.yonsei.ac.kr>
2021-03-18 22:52:10 +0900

    ãrender 사용

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # HttpResponse return 하려면 아래 코드를 써야함
+    # template = loader.get_template('polls/index.html')
     context = {'latest_question_list': latest_question_list, }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
