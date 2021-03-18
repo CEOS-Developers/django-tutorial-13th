b421e07c46e2e154007f4c854e44f9ebd63f9f06
Haejin <hj0816hj@gmail.com>
2021-03-18 14:19:58 +0900

    implemented 404 error raising

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,15 +6,14 @@ from .models import Question
 from django.http import Http404
 from django.http import HttpResponse
 # from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]  # get list of 5 recent Qs
     # output = ', '.join([q.question_text for q in latest_question_list])
     # template = loader.get_template('polls/index.html')  # loads the template and passes the context
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
+    return render(request, 'polls/index.html', context)  # shortcut for HttpResponse
 
 
 def detail(request, question_id):
@@ -28,3 +27,12 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    try:
+        # question = Question.objects.get(pk=question_id)
+        question = get_object_or_404(Question, pk=question_id)  # shortcut for Http404 exception
+    # except Question.DoesNotExist:
+        # raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
