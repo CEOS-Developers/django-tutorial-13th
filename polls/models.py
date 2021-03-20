c4c2cdd6fbbc504f4f462b15fa51f8a0e59ae5e3
최성민 <csm1165@naver.com>
2021-03-20 19:20:27 +0900

    Add remarks on models.py

--- polls/models.py
+++ polls/models.py
@@ -4,22 +4,29 @@ from django.db import models
 from django.utils import timezone
 
 
+# Question model
 class Question(models.Model):
     objects = None
+    # question_text: 질문 제목 char field
     question_text = models.CharField(max_length=200)
+    # pub_date: 발행 시간 datetime field
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
 
+    # 해당 Question model이 발행되고 하루가 지났는 지 확인
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-
+# Choice model
 class Choice(models.Model):
     DoesNotExist = None
+    # question: Choice - Question 연결
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # choice_text: 선택지 char field
     choice_text = models.CharField(max_length=200)
+    # votes : 투표수 int field
     votes = models.IntegerField(default=0)
 
     def __str__(self):
